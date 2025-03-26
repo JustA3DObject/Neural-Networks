36791b7d15c9fd1c4aec72859451b9e95cc2b7fa
Aayush <just.another.3d.object@gmail.com>
2025-03-26 19:41:28 +0530

    Added function to generate spiral dataset

--- neural_networks_from_scratch.py
+++ neural_networks_from_scratch.py
@@ -1,9 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 np.random.seed(0)
 
-X = [[1 ,2, 3, 2.5], 
-          [2.0, 5.0, -1.0, 2.0], 
-          [-1.5, 2.7, 3.3, -0.8]]
+# X = [[1 ,2, 3, 2.5], 
+#           [2.0, 5.0, -1.0, 2.0], 
+#           [-1.5, 2.7, 3.3, -0.8]]
+
+# Function for genrating data
+
+def create_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+X, y = create_data(100, 3)
+
+# plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+# plt.show()
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
